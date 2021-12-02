71bc57584385aa3a562748a162332bd965e5a1dc
Abayomi Ayodele <samabayomi301@gmail.com>
2021-12-02 23:24:45 +0100

    viewer details initial styles

--- src/components/viewer-details/ViewerDetails.tsx
+++ src/components/viewer-details/ViewerDetails.tsx
@@ -21,7 +21,28 @@ function ViewerDetailsWrapper() {
 
   console.log(data, loading, error);
 
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error loading data...</p>}
+
+      {!loading && data && (
+        <div className={styles.viewerWrapper}>
+          <div className={styles.viewerInfo}>
+            <div className={styles.imageWrapper}>
+              <img src={data.viewer.avatarUrl} alt={data.viewer.name} />
+            </div>
+
+            <h2>{data.viewer.name}</h2>
+            <h4>@{data.viewer.login}</h4>
+            <p>{data.viewer.location}</p>
+          </div>
+
+          <div className={styles.viewerRepos}></div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ViewerDetailsWrapper;
--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,35 @@
 export interface ViewerDetails {
-  avatarUrl: string;
-  login: string;
-  name: string;
-  bio: string;
-  location: string;
-  websiteUrl: string;
-
-  repositories: {
-    id: string;
+  viewer: {
+    avatarUrl: string;
+    login: string;
     name: string;
-    stargazerCount: number;
-    forkCount: number;
-    isPrivate: boolean;
-    updatedAt: Date;
-    url: string;
-    description: string;
+    bio: string;
+    location: string;
+    websiteUrl: string;
 
-    languages: {
+    repositories: {
       id: string;
       name: string;
-      color: string;
-    }[];
+      stargazerCount: number;
+      forkCount: number;
+      isPrivate: boolean;
+      updatedAt: Date;
+      url: string;
+      description: string;
 
-    repositoryTopics: {
-      topic: {
+      languages: {
+        id: string;
         name: string;
-      };
+        color: string;
+      }[];
+
+      repositoryTopics: {
+        topic: {
+          name: string;
+        };
+      }[];
     }[];
-  }[];
+  };
 }
 
 export interface ViewerDetailsVars {
